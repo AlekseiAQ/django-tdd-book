696803a1b419314f899686c37be526fe960fae67
Aleksei Kuznetcov <kuznetz.vrn@gmail.com>
2016-12-28 13:18:51 -0500

    Fron page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -26,9 +26,10 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         self.fail('Finish the test!')
