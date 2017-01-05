5fec2ed7a63f0301d4053652abc9f4877f614631
Aleksei Kuznetcov <kuznetz.vrn@gmail.com>
2017-01-04 22:37:59 -0500

    Moved Fts into their own individual files

---
+++ functional_tests/test_list_item_validation.py
@@ -0,0 +1,8 @@
+from unittest import skip
+from .base import FunctionalTest
+
+
+class ItemValidationTest(FunctionalTest):
+    """docstring for ItemValidationTest"""
+    def test_cannot_add_empty_list_items(self):
+        self.fail('write me!')
