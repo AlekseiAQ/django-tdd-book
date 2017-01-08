d38664a7520b0ae6c80f2c5583bb1a440e8b6444
Aleksei Kuznetcov <kuznetz.vrn@gmail.com>
2017-01-08 13:20:39 -0500

    use new form in home_page, simplify tests. NB breaks stuff

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -6,38 +6,23 @@ from django.template.loader import render_to_string
 
 from lists.views import home_page
 from lists.models import Item, List
+from lists.forms import ItemForm
 
 
 class HomePageTest(TestCase):
     """docstring for HomePageTest"""
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    maxDiff = None
+
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
-
-
-    # def test_saving_and_retrieving_items(self):
-    #     first_item = Item()
-    #     first_item.text = 'The first (ever) list item'
-    #     first_item.save()
-
-    #     second_item = Item()
-    #     second_item.text = 'Item the second'
-    #     second_item.save()
-
-    #     saved_items = Item.objects.all()
-    #     self.assertEqual(saved_items.count(), 2)
-
-    #     first_saved_item = saved_items[0]
-    #     second_saved_item = saved_items[1]
-    #     self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-    #     self.assertEqual(second_saved_item.text, 'Item the second')
+        expected_html = render_to_string('home.html', {'form': ItemForm()})
+        self.assertMultiLineEqual(response.content.decode(), expected_html)
 
 
 class ListViewTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,13 @@
 from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
+from lists.forms import ItemForm
 from lists.models import Item, List
 
 
+
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'form': ItemForm()})
+
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
@@ -20,6 +23,7 @@ def view_list(request, list_id):
             error = "You can't have an empty list item"
 
     return render(request, 'list.html', {'list': list_, 'error': error})
+
 
 def new_list(request):
     list_ = List.objects.create()
