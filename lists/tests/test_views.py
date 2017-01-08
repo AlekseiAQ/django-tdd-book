334e95464852285d2c2fe4739c989a384c85aad4
Aleksei Kuznetcov <kuznetz.vrn@gmail.com>
2017-01-08 18:15:05 -0500

    add unique error message

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -8,6 +8,8 @@ from lists.views import home_page
 from lists.models import Item, List
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 
+from unittest import skip
+
 
 class HomePageTest(TestCase):
     """docstring for HomePageTest"""
@@ -117,6 +119,20 @@ class ListViewTest(TestCase):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
+
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            '/lists/%d/' % (list1.id,),
+            data={'text': 'textey'}
+        )
+
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
 
 
 class NewListTest(TestCase):
