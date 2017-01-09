46f84e4deaa308add94016759bb94d546adbcef5
Aleksei Kuznetcov <kuznetz.vrn@gmail.com>
2017-01-08 20:10:48 -0500

    Implement first FT for JS

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -46,3 +46,22 @@ class ItemValidationTest(FunctionalTest):
         self.check_for_row_in_list_table('1: Buy wellies')
         error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You've already got this in your list")
+
+
+    def test_error_messages_are_cleared_on_input(self):
+        # Edith starts a new list in a way that causes a validation error:
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertTrue(error.is_displayed())
+
+        # She starts typing in the input box to clear the error
+        self.get_item_input_box().send_keys('a')
+
+        # She is pleades to see that the error message disappears
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
+
+
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
