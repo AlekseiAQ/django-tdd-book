8a0b39dc12a5e848158f9994775a115b6ead4bb5
Aleksei Kuznetcov <kuznetz.vrn@gmail.com>
2017-01-16 17:56:20 -0500

    url, placeholder view, and first-cut templates for my_lists

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -175,3 +175,11 @@ class NewListTest(TestCase):
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.client.post('/lists/new', data={'text': ''})
         self.assertIsInstance(response.context['form'], ItemForm)
+
+
+class MyListsTest(TestCase):
+    """docstring for MyListsTest"""
+    def test_my_lists_url_renders_my_lists_template(self):
+        response = self.client.get('/lists/users/a@b.com/')
+        self.assertTemplateUsed(response, 'my_lists.html')
+        
